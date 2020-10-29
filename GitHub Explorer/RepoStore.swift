3f0a87b2e7b68029491d5a8261351d3396c68bc7
Caglar <caalar@msn.com>
2020-10-28 20:29:58 -0500

    Implement the view model for fetching & parsing JSON

---
+++ GitHub Explorer/RepoStore.swift
@@ -0,0 +1,24 @@
+//
+//  RepoStore.swift
+//  GitHub Explorer
+//
+//  Created by Caglar on 10/28/20.
+//
+
+import Foundation
+
+/// RepoStore is responsible fetching and parsing the JSON data. It also publishes changes to its subscribers.
+class RepoStore: ObservableObject {
+    @Published var repos: Array<RepoModel>?
+    
+    func getRepos(for username: String) {
+        guard let url = URL(string: "https://api.github.com/users/\(username)/repos") else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            let repos = try? JSONDecoder().decode([Repo].self, from: data!)
+            DispatchQueue.main.async {
+                self.repos = repos
+            }
+        }
+        .resume()
+    }
+}
