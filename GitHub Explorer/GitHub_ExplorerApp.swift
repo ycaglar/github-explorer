d34af14b72df22e28052448db45dd038cf26a328
Caglar <caalar@msn.com>
2020-10-28 20:01:45 -0500

    Initial Commit

---
+++ GitHub Explorer/GitHub_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExplorerApp.swift
+//  GitHub Explorer
+//
+//  Created by Caglar on 10/28/20.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
