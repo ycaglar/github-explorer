bede18717728484b4cb742e8350f0112b6215737
Caglar <caalar@msn.com>
2020-10-28 22:34:12 -0500

    Add a custom color as an extension to Color and UIColor

---
+++ GitHub Explorer/Color + octocat.swift
@@ -0,0 +1,14 @@
+//
+//  Color + octocat.swift
+//  ExpSep28
+//
+//  Created by Caglar on 10/28/20.
+//
+
+import SwiftUI
+
+extension Color {
+    public static var octocat: Color {
+        return Color(UIColor(red: 3/255, green: 75/255, blue: 142/255, alpha: 1.0))
+    }
+}
---
+++ GitHub Explorer/UIColor + octocat.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor + octocat.swift
+//  ExpSep28
+//
+//  Created by Caglar on 10/28/20.
+//
+
+import UIKit
+
+extension UIColor {
+    public static var octocat: UIColor {
+        return UIColor(red: 3/255, green: 75/255, blue: 142/255, alpha: 1.0)
+    }
+}
